48dd39f43a103f1f07a67da689480ab510dc6515
bonehead555 <39053563+bonehead555@users.noreply.github.com>
2022-02-13 13:15:46 -0500

    Move Vue Components to "src/renderer"

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,37 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import path from 'path';
 
-// https://vitejs.dev/config/
+// Get the node.js current corking directory.  This should be the top of the project
+const cwd = process.cwd();
+
 export default defineConfig({
-  plugins: [vue()]
-})
+  // Vite needs to know that the root project was moved to "src/renderer"
+  root: path.join(cwd, 'src', 'renderer'),
+
+  // Support having fixed assets in "src/render/public"
+  publicDir: 'public',
+
+  // Prevent launch of a browser when the Vite server starts
+  server: {
+    open: false,
+  },
+
+  // Specify the output directory as "build/renderer"
+  // Also, empty the build directory before running a build.
+  build: {
+    outDir: path.join(cwd, 'build', 'renderer'),
+    emptyOutDir: true,
+  },
+
+  // Inform Vite about the plugins available during a build.
+  plugins: [vue()],
+
+  // Ensure that @ works as expected in imports
+  resolve: {
+    alias: {
+      '@': path.join(cwd, 'src', 'renderer')
+
+    }
+  }
+});
